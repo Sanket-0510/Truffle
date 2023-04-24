1ff5fd3b7060947d45200c49c6bfabdc4b89194d
SanketTeli <telisanket2002@gmail.com>
2023-04-24 22:38:42 +0530

    successfully deployed my solidity contract on ganache local blockchain

---
+++ contracts/p1.sol
@@ -0,0 +1,13 @@
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+contract demo{
+    uint number;
+    function set(uint  _number) public{
+     number = _number;
+    }
+    function get() public view returns (uint){
+                         return number;
+    }
+}
---
+++ migrations/2_demo_migrations.js
@@ -0,0 +1,5 @@
+const p1 = artifacts.require("demo")
+
+module.exports= function (deployer){
+                         deployer.deploy(p1);
+};
